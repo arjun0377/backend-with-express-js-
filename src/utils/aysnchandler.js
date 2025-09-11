f55f8fd2b1b79bba23d5a5006bf1469b30d4f3ec
Arjun <youremail@example.com>
2025-09-11 18:07:01 +0530

    apirespose

--- src/utils/aysnchandler.js
+++ src/utils/aysnchandler.js
@@ -1,11 +1,28 @@
 // const asynhandler = () =>{
 
+import { response } from "express"
+
 // }
+
+const asynhandler = (responsehandler)  => (res , req ,next ) =>{
+      Promise.resolve(responsehandler(req , res , next)).
+      catch(err =>next(err))
+
+      
+}
 
 
 
 export {asynhandler}
 
-const asynhandler = (fn) =>{
-
-}
+// const asynhandler = (fn) => async  (req, res, next) => {
+//   try {
+//     await fn(res , req, next)
+       
+//   } catch (error) {
+//    res.status(err.code || 500).json({
+//      success : false,
+//      message: err.message  
+//    })
+//   }
+// }
