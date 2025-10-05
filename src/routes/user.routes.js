685a3db8046f69528bd612dd567b335350f7b606
Arjun <youremail@example.com>
2025-10-05 12:32:46 +0530

    routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {  loginuser, logoutuser, registerUser } from "../controllers/user.controller.js";
+import {  changeCurrentPassword, getcurrentuser, getUserChannelProfile, loginuser, logoutuser, registerUser, updateAccountDetails, updateUserAvatar, updateUserCoverimage, userWatchHistroy } from "../controllers/user.controller.js";
 import { upload } from "../middleware/multer.middleware.js";
 import { verfyJWT } from "../middleware/auth.middleware.js";
 import { refreshAccessToken } from "../controllers/user.controller.js";
@@ -20,7 +20,14 @@ router.route("/login").post(loginuser)
 
 //secured routes
 router.route("/logout").post(verfyJWT , logoutuser)
-
 router.route("/refreshToken").post(refreshAccessToken)
+router.route("/Change-password").post(verfyJWT  , changeCurrentPassword)
+router.route("/get-Currentuser").get(getcurrentuser)
+router.route("/Update-details").patch(verfyJWT  , updateAccountDetails)
+router.route("/Update-avatar").patch(verfyJWT ,upload.single("avatar")  , updateUserAvatar)
+router.route("/Update-coverimage").patch(verfyJWT  , upload.single("coverimage"),  updateUserCoverimage)
+router.route("/c/:username").get(verfyJWT  , getUserChannelProfile)
+router.route("/get-user-watchhistory").get(verfyJWT  , userWatchHistroy)
+
 
 export default router
